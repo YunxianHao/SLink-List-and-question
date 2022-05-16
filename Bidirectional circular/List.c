1d52e1b780dcb348a5aef4aa959beeb38ba16a37
YunxianHao <92800800+YunxianHao@users.noreply.github.com>
2022-05-16 11:58:41 +0800

    Create List.c

---
+++ Bidirectional circular/List.c
@@ -0,0 +1,148 @@
+#include "List.h"
+
+LTNode* BuyListNode(LTDataType x)
+{
+	LTNode* node = (LTNode*)malloc(sizeof(LTNode));
+	if (node == NULL)
+	{
+		perror("malloc fail");
+		exit(-1);
+	}
+
+	node->data = x;
+	node->next = NULL;
+	node->prev = NULL;
+	return node;
+}
+
+//void ListInit(LTNode** pphead)
+//{
+//	*pphead = BuyListNode(-1);
+//	(*pphead)->next = *pphead;
+//	(*pphead)->prev = *pphead;
+//}
+
+LTNode* ListInit()
+{
+	LTNode* phead = BuyListNode(-1);
+	phead->next = phead;
+	phead->prev = phead;
+
+	return phead;
+}
+
+void ListPrint(LTNode* phead)
+{
+	assert(phead);
+
+	LTNode* cur = phead->next;
+	while (cur != phead)
+	{
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
+void ListPushBack(LTNode* phead, LTDataType x)
+{
+	assert(phead);
+
+	/*LTNode* newnode = BuyListNode(x);
+	LTNode* tail = phead->prev;
+	tail->next = newnode;
+	newnode->prev = tail;
+	newnode->next = phead;
+	phead->prev = newnode;*/
+
+	ListInsert(phead, x);
+
+}
+
+void ListPushFront(LTNode* phead, LTDataType x)
+{
+	assert(phead);
+
+	//LTNode* newnode = BuyListNode(x);
+	//LTNode* next = phead->next;
+
+	//// phead newnode next
+	//phead->next = newnode;
+	//newnode->prev = phead;
+	//newnode->next = next;
+	//next->prev = newnode;
+
+	ListInsert(phead->next, x);
+}
+
+//void ListPushFront(LTNode* phead, LTDataType x)
+//{
+//	assert(phead);
+//
+//	LTNode* newnode = BuyListNode(x);
+//
+//	phead->next->prev = newnode;
+//	newnode->next = phead->next;
+//
+//	phead->next = newnode;
+//	newnode->prev = phead;
+//}
+
+void ListPopBack(LTNode* phead)
+{
+	assert(phead);
+
+	assert(phead->next != phead);
+	//assert(!ListEmpty(phead));
+
+	/*LTNode* tail = phead->prev;
+	LTNode* tailPrev = tail->prev;
+
+	free(tail);
+
+	tailPrev->next = phead;
+	phead->prev = tailPrev;*/
+
+	ListErase(phead->prev);
+}
+
+void ListPopFront(LTNode* phead)
+{
+	assert(phead);
+	assert(phead->next != phead);
+
+	ListErase(phead->next);
+}
+
+bool ListEmpty(LTNode* phead)
+{
+	assert(phead);
+
+	return phead->next == phead;
+}
+
+// insert x front of the pos node
+void ListInsert(LTNode* pos, LTDataType x)
+{
+	assert(pos);
+	LTNode* prev = pos->prev;
+	LTNode* newnode = BuyListNode(x);
+
+	// prve newnode pos
+	prev->next = newnode;
+	newnode->prev = prev;
+	newnode->next = pos;
+	pos->prev = newnode;
+}
+
+//  delete pos node
+void ListErase(LTNode* pos)
+{
+	assert(pos);
+	LTNode* prev = pos->prev;
+	LTNode* next = pos->next;
+
+	prev->next = next;
+	next->prev = prev;
+	free(pos);
+}
